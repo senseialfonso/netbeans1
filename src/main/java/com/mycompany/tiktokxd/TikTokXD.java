872f3b06c480493a0390d720ea9f1a3e8bfd9fe5
SenseiAlfonso <alfonsoske@gmail.com>
2024-02-08 22:15:56 -0600

    Ejemplo Usuario y Video Java primeras clases

---
+++ src/main/java/com/mycompany/tiktokxd/TikTokXD.java
@@ -0,0 +1,23 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.tiktokxd;
+
+/**
+ *
+ * @author alfon
+ */
+public class TikTokXD {
+    public static void main(String[] args) {
+        // Crear instancias
+        Usuario usuario = new Usuario("SenseiAlfonso");
+        Video video = new Video("Poo", 60, "https://www.youtube.com/watch?v=hMZFSSF-JkQ&t=1s");
+
+        // Subir video
+        usuario.subirVideo(video);
+
+        // Mostrar información del usuario
+        usuario.mostrarInfoUsuario();
+    }
+}
